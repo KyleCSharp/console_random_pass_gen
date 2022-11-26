8c074357ebfa6c871932677cc3484e1cb6b0c0de
KyleCSharp <111399974+KyleCSharp@users.noreply.github.com>
2022-11-25 19:16:57 -0500

    finished'

---
+++ console_random_pass/Program.cs
@@ -0,0 +1,44 @@
+﻿using console_random_pass;
+using System.Diagnostics;
+using System.Text;
+
+while (true)
+{
+    Console.WriteLine("hello, welcome to my random password generator!");
+    Console.WriteLine("please type special for special character password or simple for non special character");
+    //-------------------------------------------------------------------------------------------------------------
+    var userReso = Console.ReadLine();
+    if (userReso.ToLower() == "simple")
+    {
+        Console.WriteLine("now how manys characters do you need?");
+        var numberRepo = int.Parse(Console.ReadLine());
+        
+        Console.WriteLine(rando.RandomString(numberRepo));
+
+        Console.WriteLine("now please high light and press CTRL>C to copy and paste where needed");
+    }
+    if (userReso.ToLower() == "special")
+    {
+
+        Console.WriteLine("now how many characters do you need?");
+        var numberRepo = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(rando.RandomTwo(numberRepo));
+
+        Console.WriteLine("now please high light and press CTRL>C to copy and paste where needed");
+        
+
+
+       
+    }
+    Console.WriteLine("if you would like to exit type exit or program will continue to loop");
+
+    if (userReso.ToLower() == "exit")
+    {
+        Environment.Exit(0);
+    }
+
+
+
+
+}
---
+++ console_random_pass/rando.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_random_pass
+{
+    public class rando
+    {
+
+
+        private static Random random = new Random();
+
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+
+
+        private static Random randomSpec = new Random();
+
+        public static string RandomTwo(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@!#$%";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
+
+
+    }
+}
